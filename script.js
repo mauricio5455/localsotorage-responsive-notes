fc4cf761f0f05d5a5e2b4ce1421246b7cdecc7b6
mauricio5455 <mauricio.santos81005452@gmail.com>
2023-06-10 01:29:25 -0300

    uma parte feita

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+// Coisas simpes que não necessitão de classes
+
+let add = document.querySelector('#add');
+
+add.addEventListener('click', (e) => {
+    add.classList.add('exp');
+});
