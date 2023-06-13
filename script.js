f6b054bc6a040d8b8587cb2b2fbf2a21156e7c9a
mauricio5455 <mauricio.santos81005452@gmail.com>
2023-06-13 20:32:17 -0300

    Botão voltar funcional

--- script.js
+++ script.js
@@ -6,9 +6,31 @@
 
 // Coisas simpes que não necessitão de classes
 
-let add = document.querySelector('#add');
+const add = document.querySelector('#add');
+const camponota = document.querySelector('#camponota');
+const addIcon = document.querySelector('#addicon');
+const saveBtn = document.querySelector('#save');
+const backBtn = document.querySelector('#back');
+const campoTexto = document.querySelector('#nota');
+const campoNome = document.querySelector('#nome');
 
-add.addEventListener('click', (e) => {
+add.addEventListener('click', (e) =>{
     add.classList.add('exp');
     add.style.display = 'block';
+    addicon.classList.add('sem');
+    camponota.classList.remove('sem');
 });
+
+backBtn.addEventListener('click', (e) => {
+    add.classList.remove('exp');
+    add.style.display = 'flex';
+    addIcon.classList.remove('sem');
+    camponota.classList.add('sem');
+    campoNome.value = '';
+    campoTexto.value = '';
+    e.stopPropagation();
+});
+
+function abrirNotas() {
+    
+}
