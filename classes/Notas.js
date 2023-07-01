b56d94dbb239c1220c87028f6d4cd1774dcabbd3
mauricio5455 <mauricio.santos81005452@gmail.com>
2023-07-01 17:56:19 -0300

    nota
    Agora a nota é adicionada via javascript

--- classes/Notas.js
+++ classes/Notas.js
@@ -85,9 +85,74 @@ export default class NoteHandler {
         divNota.appendChild(divTitulo);
         divNota.appendChild(divContent);
         divNota.addEventListener('click', (e) => {
-            
+            e.stopImmediatePropagation();
+            this.criarNotaAberta(e.currentTarget.id);
         });
 
         return(divNota);
+    }
+
+    criarNotaAberta(id) {
+        // Cria o elemento <div> principal
+        var divPrincipal = document.createElement("div");
+        divPrincipal.className = "notaberta";
+
+        // Cria o elemento <div> interna
+        var divInterna = document.createElement("div");
+        divInterna.className = "notabertadiv";
+
+        // Cria o elemento <div> para o nome da nota aberta
+        var divNomeNota = document.createElement("div");
+        divNomeNota.className = "nomenotaberta";
+
+        // Cria o elemento <input> para o nome da nota aberta
+        var inputNomeNota = document.createElement("input");
+        inputNomeNota.id = "namenotaopened";
+        inputNomeNota.value = JSON.parse(this.notas)[id].nome;
+
+        // Adiciona o elemento <input> ao elemento <div> do nome da nota aberta
+        divNomeNota.appendChild(inputNomeNota);
+
+        // Cria o elemento <div> para a nota aberta
+        var divNotaAberta = document.createElement("div");
+        divNotaAberta.className = "notanotaberta";
+
+        // Cria o elemento <textarea> para a nota aberta
+        var textareaNotaAberta = document.createElement("textarea");
+        textareaNotaAberta.id = "notaopened";
+        textareaNotaAberta.value = JSON.parse(this.notas)[id].nota;
+
+        // Adiciona o elemento <textarea> ao elemento <div> da nota aberta
+        divNotaAberta.appendChild(textareaNotaAberta);
+
+        // Cria o elemento <div> para os botões
+        var divBotoes = document.createElement("div");
+        divBotoes.className = "btnota";
+
+        // Cria o botão "Voltar"
+        var btnVoltar = document.createElement("button");
+        btnVoltar.id = "backnote";
+        btnVoltar.className = "notebtn";
+        btnVoltar.textContent = "Voltar";
+
+        // Cria o botão "Salvar"
+        var btnSalvar = document.createElement("button");
+        btnSalvar.id = "sabenote";
+        btnSalvar.className = "notebtn";
+        btnSalvar.textContent = "Salvar";
+
+        // Adiciona os botões ao elemento <div> dos botões
+        divBotoes.appendChild(btnVoltar);
+        divBotoes.appendChild(btnSalvar);
+
+        // Adiciona os elementos criados à hierarquia correta
+        divInterna.appendChild(divNomeNota);
+        divInterna.appendChild(divNotaAberta);
+        divInterna.appendChild(divBotoes);
+        divPrincipal.appendChild(divInterna);
+
+        // Adiciona o elemento principal ao documento HTML
+        document.body.appendChild(divPrincipal);
+
     }
 }
